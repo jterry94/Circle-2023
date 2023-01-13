4fcaf47055787a44d68f9d85d185f8c4bc169506
jterry94 <terryj@iit.edu>
2023-01-13 14:21:43 -0600

    Initial Commit

---
+++ Circle/CircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleApp.swift
+//  Circle
+//
+//  Created by Jeff_Terry on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
